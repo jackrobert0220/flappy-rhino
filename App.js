7c22612526526e6f016ebb6122ef2d883eb7371d
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-05 14:42:31 -0700

    first commit with expo running

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { GameEngine } from 'react-native-game-engine';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-    <GameEngine>
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
-      }}
-    </GameEngine>
+    <View style={{ flex: 1 }}>
+      <GameEngine
+        style=
+        {{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+        >
+      </GameEngine>
 
-      <StatusBar style="auto" hidden={true}/>
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
