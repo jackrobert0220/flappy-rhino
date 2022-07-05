cba9a7694fbc809013c830aad80f11e4ac1b2852
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-05 15:56:12 -0700

    created bird component

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
-
+import entities from "./entities";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <GameEngine
-        style=
-        {{
+        entities={entities()}
+        style={{
           position: "absolute",
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
         }}
-        >
-      </GameEngine>
+      ></GameEngine>
 
       <StatusBar style="auto" hidden={true} />
     </View>
