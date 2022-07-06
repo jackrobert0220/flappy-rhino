baea7a8066c3e8da164076a3b17a80df7ec639c0
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-06 01:04:51 -0700

    added text

--- App.js
+++ App.js
@@ -8,14 +8,25 @@ import Physics from "./physics";
 export default function App() {
   const [running, setrunning] = useState(false);
   const [gameEngine, setGameEngine] = useState(null);
-  const [currentPoints, setCurrentPoints] = useState(0)
+  const [currentPoints, setCurrentPoints] = useState(0);
   useEffect(() => {
     setrunning(false);
   }, []);
 
   return (
     <View style={{ backgroundColor: "#212121", flex: 1 }}>
-    <Text style={{ textAlign: 'center', fontSize: 50, fontWeight: 'bold', margin: 40, color: 'white'}}>{currentPoints}</Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 50,
+          fontWeight: "bold",
+          margin: 40,
+          color: "white",
+          zIndex: 2,
+        }}
+      >
+        {currentPoints}
+      </Text>
       <GameEngine
         ref={(ref) => {
           setGameEngine(ref);
@@ -27,10 +38,10 @@ export default function App() {
           switch (e.type) {
             case "game_over":
               setrunning(false);
-              gameEngine.stop()
+              gameEngine.stop();
               break;
-            case 'new_point': 
-              setCurrentPoints(currentPoints + 1)
+            case "new_point":
+              setCurrentPoints(currentPoints + 1);
               break;
           }
         }}
@@ -42,26 +53,52 @@ export default function App() {
           bottom: 0,
         }}
       >
-
-      <StatusBar style="auto" hidden={true} />
-
+        <StatusBar style="auto" hidden={true} />
       </GameEngine>
 
-        {!running ? 
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <TouchableOpacity style={{backgroundColor: 'white', paddingVertical: 20, paddingHorizontal: 40, borderRadius: 8, borderWidth: 5}}
-          onPress={() => {
-            setCurrentPoints(0)
-            setrunning(true)
-            gameEngine.swap(entities())
-          }}>
-            <Text style={{fontWeight: 'bold', fontSize: 30}}>
-              START GAME
-            </Text>
-
+      {!running ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "white",
+              paddingVertical: 20,
+              paddingHorizontal: 40,
+              borderRadius: 8,
+              borderWidth: 5,
+            }}
+            onPress={() => {
+              setCurrentPoints(0);
+              setrunning(true);
+              gameEngine.swap(entities());
+            }}
+          >
+            <Text style={{ fontWeight: "bold", fontSize: 30 }}>START GAME</Text>
           </TouchableOpacity>
-
-        </View> : null}
+          <View
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 30,
+              borderRadius: 8,
+              marginTop: 70,
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 30,
+                textShadowColor: "rgba(0, 0, 0, 0.75)",
+                textShadowOffset: { width: -1, height: 1 },
+                textShadowRadius: 10,
+              }}
+            >
+              @PlasticRhinoBand Artwork by: @MCMorrisDesign
+            </Text>
+          </View>
+        </View>
+      ) : null}
     </View>
   );
 }
