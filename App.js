cacd206d23fee8d71f5dc4dd8d71688608a24233
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-05 18:06:13 -0700

    added gravity

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from "expo-status-bar";
+import React, {useState, useEffect} from "react";
 import { View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import entities from "./entities";
+import Physics from "./physics";
+
 
 export default function App() {
+  const [running, setrunning] = useState(false)
+  useEffect(() => {
+    setrunning(true)
+  }, [])
+  
   return (
     <View style={{ flex: 1 }}>
       <GameEngine
+        systems={[Physics]}
         entities={entities()}
+        running={running}
         style={{
           position: "absolute",
           top: 0,
